6061c0a7085fff480e1ea8a8b5e9e3e0e46b075c
Matthew Lam <minghinmatthew.lam@gmail.com>
2026-09-18 16:02:39 -0400

    Update pi-gui cask to 0.1.0-beta.39

--- Casks/pi-gui.rb
+++ Casks/pi-gui.rb
@@ -2,10 +2,10 @@
 # frozen_string_literal: true
 
 cask "pi-gui" do
-  version "0.1.0-beta.33"
-  sha256 "584b7681df9b24504c208206be2588325a217d428faa1b8d9570d6721dfe0f6c"
+  version "0.1.0-beta.39"
+  sha256 "6f5b1949042bd82287ebe90d28e5789ce9af0262260693b122a3a92ca7f10e21"
 
-  url "https://github.com/minghinmatthewlam/pi-gui/releases/download/v0.1.0-beta.33/pi-gui-0.1.0-beta.33-arm64.dmg"
+  url "https://github.com/minghinmatthewlam/pi-gui/releases/download/v0.1.0-beta.39/pi-gui-0.1.0-beta.39-arm64.dmg"
   name "pi-gui"
   desc "Codex-style desktop shell for pi"
   homepage "https://github.com/minghinmatthewlam/pi-gui"
